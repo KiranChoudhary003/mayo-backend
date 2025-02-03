cf7118d10690ea68875b3990864c6027f68890b3
Kiran Choudhary <kiranchoudhary9180@gmail.com>
2025-02-03 14:02:35 +0530

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -6,11 +6,19 @@ import notificationRoutes from './src/routes/notificationRoutes.mjs';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "*" })); 
+const allowedOrigins = ["https://push-notification-murex-three.vercel.app", "https://my-app-l6fq.vercel.app"]
+app.use(cors({ 
+  origin: allowedOrigins, 
+  methods: ['GET', 'POST', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type'] 
+}));
+
 app.use(express.json());
 
 initializeFirebase();
 
 app.use('/api', notificationRoutes);
 
-app.listen(PORT, (err) =>  err ? console.log(`An error occurred: ${err}`) : console.log(`Server is running on PORT: ${PORT}`))
+app.listen(PORT, (err) => 
+  err ? console.log(`An error occurred: ${err}`) : console.log(`Server is running on PORT: ${PORT}`)
+);
