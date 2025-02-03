114df0b19f24c9b205d85701ee3fd3ae6f4d01b4
Kiran Choudhary <kiranchoudhary9180@gmail.com>
2025-02-03 21:55:56 +0530

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,24 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import { initializeFirebase } from './src/constants/firebase.mjs';
-import notificationRoutes from './src/routes/notificationRoutes.mjs';
+import express from 'express'
+import cors from 'cors'
+import { initializeFirebase } from './src/constants/firebase.mjs'
+import notificationRoutes from './src/routes/notificationRoutes.mjs'
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express()
+const PORT = process.env.PORT || 5000
 
-const allowedOrigins = ["https://push-notification-murex-three.vercel.app", "https://my-app-l6fq.vercel.app"]
-app.use(cors({ 
-  origin: allowedOrigins, 
-  methods: ['GET', 'POST', 'OPTIONS'], 
-  allowedHeaders: ['Content-Type'] 
-}));
+app.use(cors())
+app.use(express.json())
 
-app.use(express.json());
+initializeFirebase()
 
-initializeFirebase();
+app.use('/api', notificationRoutes)
 
-app.use('/api', notificationRoutes);
-
-app.listen(PORT, (err) => 
-  err ? console.log(`An error occurred: ${err}`) : console.log(`Server is running on PORT: ${PORT}`)
-);
+app.listen(PORT, (err) =>  err ? console.log(`An error occurred: ${err}`) : console.log(`Server is running on PORT: ${PORT}`))
